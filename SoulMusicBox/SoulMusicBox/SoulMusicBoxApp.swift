3c7e7e6fad6166cee8be024e274cd8a8dcd4ecda
Outlier <22833166+forchain@users.noreply.github.com>
2024-11-22 14:28:38 +0800

    feat: init project

---
+++ SoulMusicBox/SoulMusicBox/SoulMusicBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoulMusicBoxApp.swift
+//  SoulMusicBox
+//
+//  Created by Tony Outlier on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SoulMusicBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoulMusicBox/SoulMusicBoxTests/SoulMusicBoxTests.swift
@@ -0,0 +1,16 @@
+//
+//  SoulMusicBoxTests.swift
+//  SoulMusicBoxTests
+//
+//  Created by Tony Outlier on 11/22/24.
+//
+
+import Testing
+
+struct SoulMusicBoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
